ccd8faff3bf758b99c287df210308ffe3331dbf9
SirrahErydya <kollasch@uni-bremen.de>
2019-08-06 17:20:22 +0200

    Naive Convolutional network

--- network.py
+++ network.py
@@ -3,16 +3,20 @@ import torch.nn as nn
 
 
 class CharacterClassifier(nn.Module):
-
-    def __init__(self, input_dim):
+    def __init__(self, input_dim, hidden_layers, output_dim=18):
         super().__init__()
         # We got 18 characters in total: (0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ,+, -, *, /, x, y, m, =)
-        output_dim = 18
-        hidden_dim = 50
-
+        channels, height, width = input_dim
         self.layers = []
-        self.layers.append(nn.Linear(input_dim, hidden_dim))
-        self.layers.append(nn.Linear(hidden_dim, output_dim))
+        self.layers.append(nn.Conv2d(channels, height*width, (5,5)))
+        height, width = (1 + (height - 5), 1 + (width - 5))
+        last_idx = len(hidden_layers)-1
+        for i in range(last_idx):
+            self.layers.append(nn.Conv2d(hidden_layers[i], hidden_layers[i+1], (2,2)))
+            height, width = (1 + (height - 2), 1 + (width - 2))
+        self.layers.append(nn.Linear(hidden_layers[last_idx]*height*width, 120))
+        self.layers.append(nn.Linear(120, 84))
+        self.layers.append(nn.Linear(84, output_dim))
 
         self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
