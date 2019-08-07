f010f90614d1fbeb38381887ba17f22ea7fe2b90
SirrahErydya <kollasch@uni-bremen.de>
2019-08-07 09:05:23 +0200

    Add Jupyter notebook to test classifier

--- network.py
+++ network.py
@@ -3,26 +3,30 @@ import torch.nn as nn
 
 
 class CharacterClassifier(nn.Module):
+
     def __init__(self, input_dim, hidden_layers, output_dim=18):
-        super().__init__()
+        super(CharacterClassifier, self).__init__()
         # We got 18 characters in total: (0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ,+, -, *, /, x, y, m, =)
         channels, height, width = input_dim
-        self.layers = []
-        self.layers.append(nn.Conv2d(channels, height*width, (5,5)))
+        layers = []
+        layers.append(nn.Conv2d(channels, height*width, (5,5)))
+        layers.append(nn.ReLU())
+        layers.append(nn.Conv2d(height * width, hidden_layers[0], (5, 5)))
         height, width = (1 + (height - 5), 1 + (width - 5))
-        last_idx = len(hidden_layers)-1
-        for i in range(last_idx):
-            self.layers.append(nn.Conv2d(hidden_layers[i], hidden_layers[i+1], (2,2)))
+        layers.append(nn.ReLU())
+        height, width = (1 + (height - 5), 1 + (width - 5))
+        for i in range(1, len(hidden_layers)):
+            layers.append(nn.Conv2d(hidden_layers[i-1], hidden_layers[i], (2,2)))
+            layers.append(nn.ReLU())
             height, width = (1 + (height - 2), 1 + (width - 2))
-        self.layers.append(nn.Linear(hidden_layers[last_idx]*height*width, 120))
-        self.layers.append(nn.Linear(120, 84))
-        self.layers.append(nn.Linear(84, output_dim))
 
-        self.relu = nn.ReLU()
+        self.out_layer = nn.Linear(hidden_layers[len(hidden_layers)-1]*height*width, 10)
+        self.layers = nn.Sequential(*layers)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         out = x
         for layer in self.layers:
-            out = self.relu(layer(out))
-        return self.sigmoid(out)
+            out = layer(out)
+        out = out.reshape(out.shape[0], out.shape[1]*out.shape[2]*out.shape[3])
+        return self.sigmoid(self.out_layer(out))
