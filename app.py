ba46df2e141ddc81319e2f03878444e7d96689d5
DucAnhPhi <ducanhphi1@gmail.com>
2019-08-21 12:34:46 +0200

    add function to read from MOV file

--- app.py
+++ app.py
@@ -5,58 +5,8 @@ from segmentation import Segmentation
 
 
 class App:
-    def run_with_webcam(self):
-        cap = cv.VideoCapture(0)
-        while(True):
-            # Capture frame-by-frame
-            _, frame = cap.read()
-
-            # Our operations on the frame come here
-
-            # define window of interest
-            fHeight, fWidth, _ = frame.shape
-            window = (100, 100, fWidth-200, fHeight-200)
-            wX, wY, wWidth, wHeight = window
-
-            # draw window
-            cv.rectangle(frame, (wX, wY), (wX+wWidth,
-                                           wY+wHeight), (0, 0, 255), 2)
-
-            # crop frame according to defined window
-            cropped = frame[wY:wY+wHeight, wX:wX+wWidth]
-
-            # preprocess cropped image
-            preprocessed = PreProcessing().preprocess(cropped)
-
-            # find contours using algorithm by Suzuki et al. (1985)
-            contours, hierarchy = cv.findContours(
-                preprocessed, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
-
-            # get bounding boxes of contours and filter them
-            filtered = Segmentation().filter_contours(preprocessed, contours, hierarchy)
-            rects = [cv.boundingRect(cnt) for cnt in filtered]
-
-            # draw each bounding box
-            for rect in rects:
-                rX, rY, rWidth, rHeight = rect
-                rX = rX + wX
-                rY = rY + wY
-                cv.rectangle(frame, (rX, rY), (rX+rWidth,
-                                               rY+rHeight), (0, 255, 0), 2)
-
-            # Display the resulting frame
-            cv.imshow('frame', frame)
-
-            # Press ESC to quit
-            if cv.waitKey(1) == 27:
-                break
-
-        # When everything done, release the capture
-        cap.release()
-        cv.destroyAllWindows()
-
-    def run_with_img(self):
-        frame = cv.imread('sample.jpg', 1)
+    def process(self, frame):
+        # Our operations on the frame come here
 
         # define window of interest
         fHeight, fWidth, _ = frame.shape
@@ -64,7 +14,8 @@ class App:
         wX, wY, wWidth, wHeight = window
 
         # draw window
-        cv.rectangle(frame, (wX, wY), (wX+wWidth, wY+wHeight), (0, 0, 255), 2)
+        cv.rectangle(frame, (wX, wY), (wX+wWidth,
+                                       wY+wHeight), (0, 0, 255), 2)
 
         # crop frame according to defined window
         cropped = frame[wY:wY+wHeight, wX:wX+wWidth]
@@ -87,13 +38,67 @@ class App:
             rY = rY + wY
             cv.rectangle(frame, (rX, rY), (rX+rWidth,
                                            rY+rHeight), (0, 255, 0), 2)
+        # preprocessing()
+        # BorderRemoval()
+        # Segmentation()
+        # LineFitting()
+
+    def run_with_webcam(self):
+        cap = cv.VideoCapture(0)
+        while(True):
+
+            # Capture frame-by-frame
+            _, frame = cap.read()
+
+            # Processing the frame
+            self.process(frame)
+
+            # Display the resulting frame
+            cv.imshow('frame', frame)
+
+            # Press ESC to quit
+            if cv.waitKey(1) == 27:
+                break
+
+        # When everything done, release the capture
+        cap.release()
+        cv.destroyAllWindows()
+
+    def run_with_img(self):
+        frame = cv.imread('sample.jpg', 1)
+
+        self.process(frame)
 
         # Display the resulting frame
         cv.imshow('frame', frame)
         cv.waitKey(0)
         cv.destroyAllWindows()
 
+    def run_with_video(self, file):
+        cap = cv.VideoCapture(file)
+
+        if cap.isOpened():
+            while(True):
+
+                # Capture frame-by-frame
+                _, frame = cap.read()
+
+                # Processing the frame
+                self.process(frame)
+
+                # Display the resulting frame
+                cv.imshow('frame', frame)
+
+                # Press ESC to quit
+                if cv.waitKey(1) == 27:
+                    break
+
+        # When everything done, release the capture
+        cap.release()
+        cv.destroyAllWindows()
+
 
 if __name__ == '__main__':
-    App().run_with_webcam()
+    # App().run_with_webcam()
     # App().run_with_img()
+    App().run_with_video('sample.MOV')
